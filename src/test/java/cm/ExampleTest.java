f8781b5c75fe3a7f84e2a6d4589401d825325f99
Ivan <ivan.yaremko@gmail.com>
2022-03-29 19:52:05 +0100

    init commit before starting task 3

---
+++ src/test/java/cm/ExampleTest.java
@@ -0,0 +1,13 @@
+package cm;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+    void name() {
+        Assertions.assertEquals(1, 1);
+    }
+    
+}
